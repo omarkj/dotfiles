a4aa97f1867db5f88cb5e67f91d28bec17b71477
Ómar Kjartan Yasin <omarkj@gmail.com>
2013-02-05 10:36:15 +0000

    serve, aliases, tmux, zshrc

---
+++ aliases/init.zsh
@@ -0,0 +1,51 @@
+search_contacts () {
+    if [ $1 != "" ] ; then
+	google contacts list $1|awk -F',' '{print $1 " <"$2">"}'
+    fi
+}
+
+extract () {
+    if [ -f $1 ] ; then
+      case $1 in
+        *.tar.bz2)   tar xjf $1     ;;
+        *.tar.gz)    tar xzf $1     ;;
+        *.bz2)       bunzip2 $1     ;;
+        *.rar)       unrar e $1     ;;
+        *.gz)        gunzip $1      ;;
+        *.tar)       tar xf $1      ;;
+        *.tbz2)      tar xjf $1     ;;
+        *.tgz)       tar xzf $1     ;;
+        *.zip)       unzip $1       ;;
+        *.Z)         uncompress $1  ;;
+        *.7z)        7z x $1        ;;
+        *)     echo "'$1' cannot be extracted via extract()" ;;
+         esac
+     else
+         echo "'$1' is not a valid file"
+     fi
+}
+
+function create_tunnel()
+{
+  if [ $# -eq 3 ]
+  then
+    user=$1
+    host=$2
+    localPort=$3
+    remotePort=$3
+  else
+    if [ $# -eq 4 ]
+    then
+      user=$1
+      host=$2
+      localPort=$3
+      remotePort=$4
+    else
+      echo -n "User: "; read user
+      echo -n "host: "; read host
+      echo -n "Distant host: "; read remotePort
+      echo -n "Local port: "; read localPort
+    fi
+  fi
+  ssh -N -f $user@$host -L ${localPort}:${host}:${remotePort}
+}
